995b9d2d34a3351f96f419ab392701f4cd0cd31a
Parsons <Jordan.Parsons79@ed.cna.nl.ca>
2019-06-04 12:46:00 -0230

    removed outdated score code

--- scripts/game.js
+++ scripts/game.js
@@ -23,38 +23,6 @@ var postNewScores = function(scoresObject) {
     req.setRequestHeader("Content-type", "application/json");
     req.setRequestHeader("secret-key", "$2a$10$i1CbSe3/MA5qTHWLYi10h.aFktmulIwidRSjv8d3pz1hVlCWQC476");
     req.setRequestHeader("versioning", "false");
-    scoresObject.scores.third = "Devi - 8";
+    scoresObject.scores.fourth = "Hai - 7";
     req.send(JSON.stringify(scoresObject));
 }
-
-/*fetch('https://api.jsonbin.io/b/5cf6808c3185c64c762c9961', {
-    headers:{
-      'Content-Type': 'application/json',
-      'secret-key': '$2a$10$i1CbSe3/MA5qTHWLYi10h.aFktmulIwidRSjv8d3pz1hVlCWQC476'
-    }
-  })
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    scoresObject = myJson;
-    scoresObject.scores.first = "Kyle - 11";
-    return scoresObject;
-  })
-  .then(function(scoresObject) {
-    postNewScores(scoresObject);
-  });
-
-var postNewScores = function(scoresObject) {
-fetch('https://api.jsonbin.io/b/5cf6808c3185c64c762c9961', {
-  method: 'PUT',
-  headers:{
-    'Content-Type': 'application/json',
-    'secret-key': '$2a$10$i1CbSe3/MA5qTHWLYi10h.aFktmulIwidRSjv8d3pz1hVlCWQC476',
-    'versioning': 'false'
-  },
-  body: JSON.stringify(scoresObject)
-}).then(res => res.json())
-.then(response => console.log('Success:', JSON.stringify(response)))
-.catch(error => console.error('Error:', error));
-};*/
