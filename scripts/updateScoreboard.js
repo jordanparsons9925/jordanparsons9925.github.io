0e52ccc27e675a077f10607d9c5be8e9c3efc993
Jordan Parsons <49204159+jordanparsons9925@users.noreply.github.com>
2019-06-15 20:04:50 -0230

    changed bin

--- scripts/updateScoreboard.js
+++ scripts/updateScoreboard.js
@@ -99,7 +99,7 @@ req.onreadystatechange = () => {
   }
 };
 
-req.open("GET", "https://api.jsonbin.io/b/5cf68a2ee36bab4cf31198d8", true);
+req.open("GET", "https://api.jsonbin.io/b/5d0572024f234842a5661da9", true);
 req.setRequestHeader("secret-key", "$2a$10$i1CbSe3/MA5qTHWLYi10h.aFktmulIwidRSjv8d3pz1hVlCWQC476");
 req.send();
 
@@ -112,7 +112,7 @@ var postNewScores = function(scoresObject) {
         }
       };
       
-    req.open("PUT", "https://api.jsonbin.io/b/5cf68a2ee36bab4cf31198d8", true);
+    req.open("PUT", "https://api.jsonbin.io/b/5d0572024f234842a5661da9", true);
     req.setRequestHeader("Content-type", "application/json");
     req.setRequestHeader("secret-key", "$2a$10$i1CbSe3/MA5qTHWLYi10h.aFktmulIwidRSjv8d3pz1hVlCWQC476");
     req.setRequestHeader("versioning", "false");
